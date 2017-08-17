0c73a334fc070b9912e08d16d0632f05739a36b3
liuyt <liuyt@atomtree.cn>
2017-08-17 14:55:28 +0800

    jsut fmt

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -10,15 +10,18 @@ import (
 var (
 	debug = false
 )
+
 // SetDebug  make Mutex timing for Lock
 func SetDebug(d bool) {
 	debug = d
 }
+
 // Mutex which extends sync.Mutex with the ability to time the Lock operation
 type Mutex struct {
 	sync.Mutex
 	waited int64 //等待时间，毫秒
 }
+
 // Lock fetch the Lock, if debug is true, sum the costing time
 func (m *Mutex) Lock() {
 	if debug {
@@ -30,6 +33,7 @@ func (m *Mutex) Lock() {
 		m.Mutex.Lock()
 	}
 }
+
 // Unlock just unlock
 func (m *Mutex) Unlock() {
 	m.Mutex.Unlock()
@@ -40,6 +44,7 @@ func (m *Mutex) Waited() time.Duration {
 	d, _ := time.ParseDuration(fmt.Sprintf("%dms", atomic.LoadInt64(&m.waited)))
 	return d
 }
+
 //Reset reset the timer
 func (m *Mutex) Reset() {
 	atomic.StoreInt64(&m.waited, 0)
