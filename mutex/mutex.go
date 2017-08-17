c1255d6a837f5d4b470f0822013053790eff77cd
liuyt <liuyt@atomtree.cn>
2017-08-17 14:53:27 +0800

    add the comment the exported functions in package mutex

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -10,16 +10,16 @@ import (
 var (
 	debug = false
 )
-
+// SetDebug  make Mutex timing for Lock
 func SetDebug(d bool) {
 	debug = d
 }
-
+// Mutex which extends sync.Mutex with the ability to time the Lock operation
 type Mutex struct {
 	sync.Mutex
 	waited int64 //等待时间，毫秒
 }
-
+// Lock fetch the Lock, if debug is true, sum the costing time
 func (m *Mutex) Lock() {
 	if debug {
 		start := time.Now()
@@ -30,16 +30,17 @@ func (m *Mutex) Lock() {
 		m.Mutex.Lock()
 	}
 }
-
+// Unlock just unlock
 func (m *Mutex) Unlock() {
 	m.Mutex.Unlock()
 }
-//Waited() get the waited time cost by Lock()
+
+//Waited get the waited time cost by Lock()
 func (m *Mutex) Waited() time.Duration {
 	d, _ := time.ParseDuration(fmt.Sprintf("%dms", atomic.LoadInt64(&m.waited)))
 	return d
 }
-
+//Reset reset the timer
 func (m *Mutex) Reset() {
 	atomic.StoreInt64(&m.waited, 0)
 }
--- mutex/mutex_test.go
+++ mutex/mutex_test.go
@@ -5,8 +5,9 @@ import (
 	"testing"
 	"time"
 )
+
 var (
-	result=1
+	result = 1
 )
 
 func TestMutex_Waited(t *testing.T) {
@@ -33,14 +34,14 @@ func TestMutex_Waited(t *testing.T) {
 func BenchmarkMutex_Lock(b *testing.B) {
 	b.Run("sync.Mutex", func(b *testing.B) {
 		wg := &sync.WaitGroup{}
-		m:=&sync.Mutex{}
-		for i:=0;i<20;i++{
+		m := &sync.Mutex{}
+		for i := 0; i < 20; i++ {
 			go func() {
 				wg.Add(1)
 				m.Lock()
 
 				time.Sleep(time.Millisecond * 100)
-				result=2
+				result = 2
 				m.Unlock()
 				wg.Done()
 			}()
@@ -49,14 +50,14 @@ func BenchmarkMutex_Lock(b *testing.B) {
 	})
 	b.Run("Mutex", func(b *testing.B) {
 		wg := &sync.WaitGroup{}
-		m:=&Mutex{}
-		for i:=0;i<20;i++{
+		m := &Mutex{}
+		for i := 0; i < 20; i++ {
 			go func() {
 				wg.Add(1)
 				m.Lock()
 
 				time.Sleep(time.Millisecond * 100)
-				result=2
+				result = 2
 
 				m.Unlock()
 				wg.Done()
